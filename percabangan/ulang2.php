cb4ecbec3dcc0feecd79e29cfe5aa6509c52259a
RahmatArdianzah <2131710113@student.polinema.ac.id>
2022-04-03 11:57:42 +0700

    Percabangan & perulangan

---
+++ percabangan/ulang2.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <?php
+            $colors = array ("red", "green", "blue", "yelllow");
+            foreach ($colors as $value){
+                echo "$value <br>";
+            }
+        ?>
+    </body>
+</html>
