72a2bcda81d71604698294a7daf86ae8944c865a
RahmatArdianzah <2131710113@.polinema.ac.id>
2022-05-25 17:08:03 +0700

    Minggu11 SQL2

---
+++ Minggu11/homeCRUD.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>
+            Home
+        </title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <h1>DATA MAHASISWA</h1>
+        <form action="search.php" method="get">
+            Cari data mahasiswa berdasarkan nama :
+            <input type="text" name="carinama">
+            <input type="submit" value="cari">
+        </form>
+        <br>
+        <a href="insertForm.html"><button>Tambah</button></a>
+        <a href="cetak.php"><button>Cetak Laporan</button></a>
+    </body>
+</html>
